81e1e089cebf77370dc254cdfeed5fba5de0e2b0
mparra26 <mparramendoza@byupathway>
2025-06-04 00:04:01 -0400

    update

--- script/filtered-temples.js
+++ script/filtered-temples.js
@@ -4,42 +4,48 @@ const temples = [
     location: "Salt Lake City, Utah",
     dedicated: "1893-04-06",
     area: 253015,
-    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/salt-lake-temple/salt-lake-temple-lds-1024942-wallpaper.jpg"
+    imageUrl:
+      "https://churchofjesuschristtemples.org/assets/img/temples/salt-lake-temple/salt-lake-temple-lds-1024942-wallpaper.jpg"
   },
   {
     name: "Provo City Center Temple",
     location: "Provo, Utah",
     dedicated: "2016-03-20",
     area: 85000,
-    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/provo-city-center-temple/provo-city-center-temple-lds-1487565-wallpaper.jpg"
+    imageUrl:
+      "https://churchofjesuschristtemples.org/assets/img/temples/provo-city-center-temple/provo-city-center-temple-lds-1487565-wallpaper.jpg"
   },
   {
     name: "Logan Utah Temple",
     location: "Logan, Utah",
     dedicated: "1884-05-17",
     area: 119619,
-    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/logan-utah-temple/logan-utah-temple-lds-1139653-wallpaper.jpg"
+    imageUrl:
+      "https://churchofjesuschristtemples.org/assets/img/temples/logan-utah-temple/logan-utah-temple-lds-1139653-wallpaper.jpg"
   },
   {
     name: "Paris France Temple",
     location: "Le Chesnay, France",
     dedicated: "2017-05-21",
     area: 44000,
-    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/paris-france-temple/paris-france-temple-lds-1770351-wallpaper.jpg"
+    imageUrl:
+      "https://churchofjesuschristtemples.org/assets/img/temples/paris-france-temple/paris-france-temple-lds-1770351-wallpaper.jpg"
   },
   {
     name: "Campinas Brazil Temple",
     location: "Campinas, Brazil",
     dedicated: "2002-05-17",
     area: 48400,
-    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/campinas-brazil-temple/campinas-brazil-temple-lds-1487590-wallpaper.jpg"
+    imageUrl:
+      "https://churchofjesuschristtemples.org/assets/img/temples/campinas-brazil-temple/campinas-brazil-temple-lds-1487590-wallpaper.jpg"
   },
   {
     name: "St. George Utah Temple",
     location: "St. George, Utah",
     dedicated: "1877-04-06",
     area: 110000,
-    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/st.-george-utah-temple/st.-george-utah-temple-lds-2236520-wallpaper.jpg"
+    imageUrl:
+      "https://churchofjesuschristtemples.org/assets/img/temples/st.-george-utah-temple/st.-george-utah-temple-lds-2236520-wallpaper.jpg"
   }
 ];
 
